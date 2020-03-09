484ddd51aa2ac557a68dd6aca0a49e669016eac6
Kats <20KatsNi@fps.fmtdom.local>
2020-03-09 10:08:12 -0400

    3/9

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args) {
+        Pet p1 = new Pet("Arnold");
+        System.out.println(p1.getName());
+        System.out.println(p1.speak());
+
+    }
+
+}
