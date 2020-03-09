9083e22d9e4833b071c46dd0165c80cf037b0d19
Kats <20KatsNi@fps.fmtdom.local>
2020-03-09 10:30:56 -0400

    3/9.2

--- Test.java
+++ Test.java
@@ -1,21 +1,5 @@
 public class Test{
     public static void main(String[] args) {
-        test1();
-    }
-
-    public static void test1(){
-        NickKat k1 = new NickKat();
-    }
-
-    public static void test2(){
-
-    }
-
-    public static void test3(){
-    
-    }
-
-    public static void test4(){
-
+        
     }
 }
