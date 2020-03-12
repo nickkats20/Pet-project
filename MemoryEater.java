f11ed52cf47e332f8caabc7290d1b56e126ec357
Kats <20KatsNi@fps.fmtdom.local>
2020-03-12 10:32:46 -0400

    3/152

--- MemoryEater.java
+++ MemoryEater.java
@@ -1,9 +1,31 @@
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.util.Vector;
 
 public class MemoryEater
 {
+    private static final int MEGABYTE = (1024*1024);
+    public static void runOutOfMemory() {
+        MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+        for (int i=1; i <= 100; i++) {
+            try {
+                byte[] bytes = new byte[MEGABYTE*500];
+            } catch (Exception e) {
+                e.printStackTrace();
+            } catch (OutOfMemoryError e) {
+                MemoryUsage heapUsage = memoryBean.getHeapMemoryUsage();
+                long maxMemory = heapUsage.getMax() / MEGABYTE;
+                long usedMemory = heapUsage.getUsed() / MEGABYTE;
+                System.out.println(i+ " : Memory Use :" + usedMemory + "M/" + maxMemory + "M");
+            }
+        }
+    }
+
   public static void main(String[] args)
   {
+    runOutOfMemory();
+    runOutOfMemory();
     Vector v = new Vector();
     while (true)
     {
@@ -19,5 +41,6 @@ public class MemoryEater
             System.out.println( "free memory: " + rt.freeMemory() );
         }
     }
+    
   }
 }
